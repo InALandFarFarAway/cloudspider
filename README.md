52da24cc2cd6018b3ce5451ee2a4ead3613ab481
asabanovic <asabanovic@localhost>
2015-03-02 08:11:51 +0100

    Tshark deployment to docker.

--- README.md
+++ README.md
@@ -3,3 +3,19 @@
 ## About
 
 Remote host traffic tracing.
+
+# Set up docker
+
+##Add the docker group if it doesn't already exist.
+sudo groupadd docker
+
+##Add the connected user "${USER}" to the docker group. Change the user name to match your preferred user.
+sudo gpasswd -a ${USER} docker
+
+##Restart the Docker daemon:
+sudo service docker restart
+
+##If you are on Ubuntu 14.04 and up use docker.io instead:
+sudo service docker.io restart
+
+Type nodejs index.js for installation of tshark on docker
---
+++ index.js
@@ -0,0 +1,43 @@
+var exec = require('child_process').exec;
+
+var swcommon = exec('docker run ubuntu:14.04 \n\
+apt-get install -y software-properties-common');
+swcommon.on('close', function() {
+    console.log('Software Properties Common installed.');
+    var commitSwCommon = exec ('docker commit 0a7a94 ubuntu:14.04');
+    commitSwCommon.on('close',function(){
+        console.log('Software Properties Common commited.');
+        
+        var tsharkrepo = exec ('docker run ubuntu:14.04 \n\
+            add-apt-repository ppa:n-muench/programs-ppa');
+        tsharkrepo.on('close', function(){
+            console.log('Tshark repo installed');
+            
+            var commitRepo = exec('docker commit 1ec84b ubuntu:14.04');
+            commitRepo.on('close', function(){
+                
+                var updateRepo = exec ('docker run ubuntu:14.04 \n\
+                apt-get update');
+    //<-------------------------<
+    updateRepo.on('close', function(){
+        
+        var commitUpdate = exec('docker commit 4691a ubuntu:14.04');
+        commitUpdate.on('close', function(){
+            
+            var installTshark = exec('docker run ubuntu:14.04 \n\
+            apt-get install -y tshark');
+            
+            installTshark.on('close', function(){
+                console.log('tshark is installed');
+                
+                var commitSwCommon = exec ('docker commit cbe904 ubuntu:14.04');
+                commitSwCommon.on('close', function(){
+                    console.log('Tshark successfuly installed.');
+                });             
+            });
+        });
+    });//----->               
+            });
+        });
+    });
+});
